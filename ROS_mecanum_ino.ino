6f9b4fec4ee1e468023237c708ae7e2704f78edd
shibalik <39675025+shibalik@users.noreply.github.com>
2018-06-25 18:14:44 +0530

    Update ROS_mecanum_ino.ino

--- ROS_mecanum_ino.ino
+++ ROS_mecanum_ino.ino
@@ -14,61 +14,60 @@ ros:: NodeHandle nh;
 void mecanum_cb(const std_msgs::String& string){
     if("STRAIGHT"==string.data)
     {
-      analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+      analogWrite( 5,255);
+      digitalWrite( 28,HIGH);
+      digitalWrite( 29,LOW);
       
-      analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+      analogWrite( 4,255);
+      digitalWrite( 26,HIGH);
+      digitalWrite( 27,LOW);
 
-      analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+      analogWrite( 3,255);
+      digitalWrite( 24,HIGH);
+      digitalWrite( 25,LOW);
 
-      analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+      analogWrite( 2,255);
+      digitalWrite( 22,HIGH);
+      digitalWrite( 23,LOW);
 
     }
     if("RIGHT"==string.data)
     {
-       analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+             analogWrite( 5,255);
+      digitalWrite( 28,HIGH);
+      digitalWrite( 29,LOW);
       
-            analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
-      
-            analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+      analogWrite( 4,255);
+      digitalWrite( 26,LOW);
+      digitalWrite( 27,HIGH);
 
-      analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+      analogWrite( 3,255);
+      digitalWrite( 24,HIGH);
+      digitalWrite( 25,LOW);
+
+      analogWrite( 2,255);
+      digitalWrite( 22,LOW);
+      digitalWrite( 23,HIGH);
 
 
     }
     if("LEFT"==string.data)
     {
-      analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+            analogWrite( 5,255);
+      digitalWrite( 28,LOW);
+      digitalWrite( 29,HIGH);
       
-       analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+      analogWrite( 4,255);
+      digitalWrite( 26,HIGH);
+      digitalWrite( 27,LOW);
 
-      analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
+      analogWrite( 3,255);
+      digitalWrite( 24,LOW);
+      digitalWrite( 25,HIGH);
 
-      analogWrite( ,255);
-      digitalWrite( ,HIGH);
-      digitalWrite( ,LOW);
-
+      analogWrite( 2,255);
+      digitalWrite( 22,HIGH);
+      digitalWrite( 23,LOW);
     }
 } 
 
@@ -86,4 +85,3 @@ void loop(){
   nh.spinOnce();
   delay(1);
 }
- 
